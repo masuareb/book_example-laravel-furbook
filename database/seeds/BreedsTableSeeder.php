d13a7a0b3e4decb9ce70155ea942607e55423a36
Arturo Suárez <masuareb@hotmail.com>
2021-01-28 23:26:59 -0600

    Added seed

---
+++ database/seeds/BreedsTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BreedsTableSeeder extends Seeder {
+    public function run() {
+        DB::table('breeds')->insert([
+            ['id' => 1, 'name' => "Domestic"],
+            ['id' => 2, 'name' => "Persian"],
+            ['id' => 3, 'name' => "Siamese"],
+            ['id' => 4, 'name' => "Abyssinian"],
+        ]);
+    }
+}
